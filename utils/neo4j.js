3d33b28b0204cbed6f519b2bee308a64e2a42c75
Connor Davis <19422@kloofhigh.co.za>
2022-04-26 06:56:54 +0200

    Update for EL

--- utils/neo4j.js
+++ utils/neo4j.js
@@ -4,54 +4,63 @@ let logger = require('./logger');
 let neo4jUrl = process.env.NEO4J_URL;
 
 let driver = neo4j.driver(
-    neo4jUrl,
-    neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+  neo4jUrl,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 );
 
-let writeTransaction = async (query, callback) => {
-    let session = driver.session({
-        database: process.env.NEO4J_DATABASE,
-        defaultAccessMode: neo4j.session.WRITE,
-    });
+logger.info(
+  `Database - ${neo4jUrl} (${process.env.NEO4J_USER}:${process.env.NEO4J_PASSWORD})`
+);
 
-    await session.writeTransaction((tx) =>
-        query.data
-            ? tx.run(query.statement, query.data).then(
-                (success) => {
-                    callback(null, success);
-                },
-                (error) => callback(error, null)
-            )
-            : tx.run(query.statement).then(
-                (success) => {
-                    callback(null, success);
-                },
-                (error) => callback(error, null)
-            )
-    );
+driver
+  .verifyConnectivity({ database: 'neo4j' })
+  .then((verified) => logger.info(JSON.stringify(verified)))
+  .catch((error) => logger.error(JSON.stringify(error)));
+
+let writeTransaction = async (query, callback) => {
+  let session = driver.session({
+    database: process.env.NEO4J_DATABASE,
+    defaultAccessMode: neo4j.session.WRITE,
+  });
+
+  await session.writeTransaction((tx) =>
+    query.data
+      ? tx.run(query.statement, query.data).then(
+          (success) => {
+            callback(null, success);
+          },
+          (error) => callback(error, null)
+        )
+      : tx.run(query.statement).then(
+          (success) => {
+            callback(null, success);
+          },
+          (error) => callback(error, null)
+        )
+  );
 };
 
 let readTransaction = async (query, callback) => {
-    let session = driver.session({
-        database: process.env.NEO4J_DATABASE,
-        defaultAccessMode: neo4j.session.WRITE,
-    });
+  let session = driver.session({
+    database: process.env.NEO4J_DATABASE,
+    defaultAccessMode: neo4j.session.WRITE,
+  });
 
-    await session.readTransaction((tx) =>
-        query.data
-            ? tx.run(query.statement, query.data).then(
-                (success) => {
-                    callback(null, success);
-                },
-                (error) => callback(error, null)
-            )
-            : tx.run(query.statement).then(
-                (success) => {
-                    callback(null, success);
-                },
-                (error) => callback(error, null)
-            )
-    );
+  await session.readTransaction((tx) =>
+    query.data
+      ? tx.run(query.statement, query.data).then(
+          (success) => {
+            callback(null, success);
+          },
+          (error) => callback(error, null)
+        )
+      : tx.run(query.statement).then(
+          (success) => {
+            callback(null, success);
+          },
+          (error) => callback(error, null)
+        )
+  );
 };
 
-module.exports = {writeTransaction, readTransaction};
+module.exports = { writeTransaction, readTransaction };
