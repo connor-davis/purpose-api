b7ef637d857796694101c5d26a3972558c5c1a1f
Connor Davis <19422@kloofhigh.co.za>
2022-05-21 13:06:59 +0200

    Updates 🥳

--- api/users/deleteUser.routes.js
+++ api/users/deleteUser.routes.js
@@ -1,6 +1,6 @@
-let {Router} = require('express');
-let {DELETE_USER} = require('../../queries/userQuerys');
-let {writeTransaction} = require('../../utils/neo4j');
+let { Router } = require('express');
+let { DELETE_USER } = require('../../queries/userQuerys');
+let { writeTransaction } = require('../../utils/neo4j');
 let router = Router();
 
 /**
@@ -22,17 +22,22 @@ let router = Router();
  *         description: Returns "Unauthorized".
  */
 router.delete('/:email', async (request, response) => {
-    let {params} = request;
+  let { params } = request;
 
-    await writeTransaction(DELETE_USER(params.email), (error, result) => {
-        if (error)
-            return response
-                .status(200)
-                .json({message: 'Error while deleting a user.', error});
-        else {
-            return response.status(200).send("success");
-        }
-    });
+  console.log(params);
+
+  await writeTransaction(DELETE_USER(params.email), (error, result) => {
+    if (error) {
+      console.log(error);
+
+      return response
+        .status(200)
+        .json({ message: 'Error while deleting a user.', error });
+    } else {
+      console.log(result);
+      return response.status(200).send('success');
+    }
+  });
 });
 
 module.exports = router;
